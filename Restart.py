046d3ccd05608f42f687221314d371cb1b409e00
dakshsaini1103 <dakshsaini1103@gmail.com>
2025-04-15 23:54:04 +0530

    Add files via upload

---
+++ Restart.py
@@ -0,0 +1,16 @@
+"""""
+x= float(input("enter a number? "))
+y= float(input("enter a second number? "))
+
+z=x+y
+print(f"{z:.2f}")
+
+ """
+
+
+def hello():
+    print("hello")
+
+name =input("what's your name? ").title().strip()
+hello()
+print(name)
